00daf3fe7009da817929652ee3000049ec5c80a9
Sherzod Karimov <sheraleks869@gmail.com>
2020-09-08 12:59:33 +0500

    Upgrade Transformers Project

--- works/transformers.pro/js/main.js
+++ works/transformers.pro/js/main.js
@@ -57,3 +57,30 @@ function myFunction() {
 		x.style.display = "block";
 	}
 }
+
+// Modal Window
+// Get the Modal
+var modal = document.getElementById('headerModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("leaveRequest");
+
+// Get the span element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user cliks the button, open the modal
+btn.onclick = function () {
+	modal.style.display = 'block';
+};
+
+// When the user clicks on span, close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
